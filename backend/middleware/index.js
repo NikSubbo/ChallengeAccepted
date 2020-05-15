2626db18198a13b2334f07d2cbb59c47a99fba7c
NikSubbo <nikolai.subbotin@gmail.com>
2020-05-15 11:08:51 +0300

    Project setup

---
+++ backend/middleware/index.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+  const express = require("express");
+  const morgan = require("morgan");
+  const path = require("path");
+  const dbConnection = require("./db-connect");
+
+  app.use(morgan("dev"));
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // app.use(express.static(path.join(__dirname, '..', "public")));
+
+};
