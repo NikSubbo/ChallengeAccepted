31bdbb1e6958d1a2cc58eb630f3cd17569ff1ba3
NikSubbo <nikolai.subbotin@gmail.com>
2020-05-15 11:26:35 +0300

    added components/pages/redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './App.css'
+import {
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 export class App extends Component {
   render() {
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+// import { reducer } from './redux/reducer';
+// import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
